c063e0915f238cbe991b0ef73c315378d41e4a9d
Yaeluribe <2962@holbertonschool.com>
2021-12-21 15:40:04 -0500

    updating modul

--- models/state.py
+++ models/state.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy.sql.expression import null
-from sqlalchemy.sql.schema import ForeignKey
+from AirBnB_clone_v2.models import city
+from AirBnB_clone_v2.models.city import City
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
     name = Column(String(128), nullable=False)
     __tablename__ = 'states'
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """cities with state_id == to current id"""
+            c_list = []
+            dict_obj = all(City)
+            for i in list(dict_obj).values():
+                if city.state_id == self.id:
+                    c_list.append(city)
+            return c_list
