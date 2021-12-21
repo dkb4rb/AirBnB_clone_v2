fa6203a805489e29e385f074299e8378091e78ea
Yaeluribe <2962@holbertonschool.com>
2021-12-21 15:27:43 -0500

    updating module

--- models/state.py
+++ models/state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 
 
 class State(BaseModel, Base):
     """ State class """
-    name = 
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
