5fe8515249dab0e318589152005bb3dd6c8cfd35
Yaeluribe <2962@holbertonschool.com>
2021-12-21 15:24:04 -0500

    updating class instances

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import Base, BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'cities'
