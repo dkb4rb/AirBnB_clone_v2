2cdb3c336d32d05c91d8521bddf4c76e86ff5332
Yaeluribe <2962@holbertonschool.com>
2021-12-21 15:18:02 -0500

    updated __init__ from kwargs, 'cause were sending it as kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -23,12 +23,23 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+          #  kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+          #                                          '%Y-%m-%dT%H:%M:%S.%f')
+          #  kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+          #                                        '%Y-%m-%dT%H:%M:%S.%f')
+          #  del kwargs['__class__']
+          #  self.__dict__.update(kwargs)
+          for key, value in kwargs.items():
+            if key == "updated_at" or key == "created_at":
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+            if key != "__class__":
+                setattr(self, key, value)
+            if 'id' not in kwargs.items():
+                setattr(self, 'id', str(uuid.uuid4()))
+            if 'updated_at' not in kwargs.items():
+                setattr(self, 'updated_at', datetime.now())
+            if 'created_at' not in kwargs.items():
+                setattr(self, 'created_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
