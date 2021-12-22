192081398c410fc5cd536edd0031dda653c9d555
Yaeluribe <2962@holbertonschool.com>
2021-12-22 15:26:13 -0500

    sd

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Module: test_amenity"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """Test_amenity Class"""
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """initializing instances"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """Test 2 name"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
