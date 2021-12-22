a4aab296e7947a80e75cf5702c1c3eb8ec3636df
Yaeluribe <2962@holbertonschool.com>
2021-12-21 19:20:10 -0500

    more import and changing expressions

--- models/state.py
+++ models/state.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from AirBnB_clone_v2.models import city
 from models.city import City
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    name = Column(String(128), nullable=False)
     __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
@@ -19,8 +19,8 @@ class State(BaseModel, Base):
         def cities(self):
             """City instances with state_id == to current state.id"""
             c_list = []
-            dict_obj = all(City)
+            dict_obj = models.storage.all(City)
             for i in list(dict_obj).values():
-                if city.state_id == self.id:
-                    c_list.append(city)
+                if i.state_id == self.id:
+                    c_list.append(i)
             return c_list
