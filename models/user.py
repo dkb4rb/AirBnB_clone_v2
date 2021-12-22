a53e1fce79b00f1d380bfd715e6865e217de09bf
root <root@localhost.localdomain>
2021-12-22 01:59:20 -0500

    again to 8090192091201902 solution error

--- models/user.py
+++ models/user.py
@@ -11,3 +11,13 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
+    reviews = relationship(
+        "Review",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
