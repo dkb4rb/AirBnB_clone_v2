692ca81604a1aeac2d927bedef447a91451dbcc4
Yaeluribe <2962@holbertonschool.com>
2021-12-22 15:29:00 -0500

    undoing documentation

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -5,65 +5,65 @@ from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """test_Place Class"""
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """init instances"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
     def test_city_id(self):
-        """test city.id"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
-        """test user.id"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
-        """test name"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
-        """test description"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
-        """test number of rooms"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
-        """test number of bathrooms"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
-        """test max guests"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
-        """test price by night"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
-        """test latitude"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
-        """test longitude"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
-        """test amenity"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Module: test_state"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """test_state Class"""
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """init instances"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """test name 3"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
